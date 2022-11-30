7da589ca70643b33413ca27ced256af99f3e66f8
Hayden Young <hayden.y@minertoken.io>
2022-11-30 22:43:33 +0000

    feat: Provide an ethers wrapper around native functions.

---
+++ src/wrappers/ethers.ts
@@ -0,0 +1,126 @@
+/**
+ * Provides an ethers wrapper for native Best Price Path functions.
+ */
+
+import { Token, Trade } from '@uniswap/sdk'
+import { Contract } from 'ethers'
+import { BestPriceOptions } from '../types/options'
+import * as Base from '../index'
+import { BEST_PRICE_OPTIONS, CHAIN_ID_DEFAULT } from '../constants/defaults'
+
+/**
+ * Finds the best price path for exactly the amount of tokenA.
+ *
+ * @param {Contract} tokenA The token in as an instance of ethers.Contract.
+ * @param {Contract} tokenB The token out as an instance of ethers.Contract.
+ * @param {any} exactTokenA The exact amount of token in. Is converted to a
+ * string so number libraries such as BigNumber.js can be used provided there
+ * is a toString() method available. Alternatively, provider the amount as a
+ * string.
+ * @param {any} provider A provider object compatible with the EIP-1193
+ * standard.
+ * @param {BestPriceOptions} opts One or more options for the best path
+ * calculation. Overrides the default best price options.
+ * @returns An array of token addresses representing the best price path from
+ * tokenIn to tokenOut for the exact amount of tokenIn.
+ */
+export async function findBestPathExactTokenAToTokenB (tokenA: Contract, tokenB: Contract, exactTokenA: any, provider: any, opts: BestPriceOptions = {}): Promise<string[]> {
+  const uniTokenA: Token = await fromEthersToUniswapToken(tokenA, opts)
+  const uniTokenB: Token = await fromEthersToUniswapToken(tokenB, opts)
+
+  return await Base.findBestPathExactTokenAToTokenB(uniTokenA, uniTokenB, exactTokenA, provider, opts)
+}
+
+/**
+ * Finds one or more best price paths between tokenA and tokenB for an exact
+ * amount of tokenA.
+ *
+ * @param {Contract} tokenA The token in as an instance of ethers.Contract.
+ * @param {Contract} tokenB The token out as an instance of ethers.Contract.
+ * @param {any} exactTokenA The exact amount of token in. Is converted to a
+ * string so number libraries such as BigNumber.js can be used provided there
+ * is a toString() method available. Alternatively, provider the amount as a
+ * string.
+ * @param {any} provider A provider object compatible with the EIP-1193
+ * standard.
+ * @param {BestPriceOptions} opts One or more options for the best path
+ * calculation. Overrides the default best price options.
+ * @returns A list of one or more best paths, each of which will contain an
+ * array of token addresses representing the best price path from tokenB to
+ * tokenB for the exact amount of tokenA.
+ */
+export async function computeExactTokenAToTokenB (tokenA: Contract, tokenB: Contract, exactTokenA: any, provider: any, opts: BestPriceOptions = {}): Promise<Trade[]> {
+  const uniTokenA: Token = await fromEthersToUniswapToken(tokenA, opts)
+  const uniTokenB: Token = await fromEthersToUniswapToken(tokenB, opts)
+
+  return await Base.computeExactTokenAToTokenB(uniTokenA, uniTokenB, exactTokenA, provider, opts)
+}
+
+/**
+ * Finds the best price path for exactly the amount of tokenB.
+ *
+ * @param {Contract} tokenA The token in as an instance of ethers.Contract.
+ * @param {Contract} tokenB The token out as an instance of ethers.Contract.
+ * @param {any} exactTokenB The exact amount of token in. Is converted to a
+ * string so number libraries such as BigNumber.js can be used provided there
+ * is a toString() method available. Alternatively, provider the amount as a
+ * string.
+ * @param {any} provider A provider object compatible with the EIP-1193
+ * standard.
+ * @param {BestPriceOptions} opts One or more options for the best path
+ * calculation. Overrides the default best price options.
+ * @returns An array of token addresses representing the best price path from
+ * tokenIn to tokenOut for the exact amount of tokenIn.
+ */
+export async function findBestPathTokenAToExactTokenB (tokenA: Contract, tokenB: Contract, exactTokenB: any, provider: any, opts: BestPriceOptions = {}): Promise<string[]> {
+  const uniTokenA: Token = await fromEthersToUniswapToken(tokenA, opts)
+  const uniTokenB: Token = await fromEthersToUniswapToken(tokenB, opts)
+
+  return await Base.findBestPathTokenAToExactTokenB(uniTokenA, uniTokenB, exactTokenB, provider, opts)
+}
+
+/**
+ * Finds one or more best price paths between tokenA and tokenB for an exact
+ * amount of tokenB.
+ *
+ * @param {Contract} tokenA The token in as an instance of ethers.Contract.
+ * @param {Contract} tokenB The token out as an instance of ethers.Contract.
+ * @param {any} exactTokenB The exact amount of token in. Is converted to a
+ * string so number libraries such as BigNumber.js can be used provided there
+ * is a toString() method available. Alternatively, provider the amount as a
+ * string.
+ * @param {any} provider A provider object compatible with the EIP-1193
+ * standard.
+ * @param {BestPriceOptions} opts One or more options for the best path
+ * calculation. Overrides the default best price options.
+ * @returns A list of one or more best paths, each of which will contain an
+ * array of token addresses representing the best price path from tokenA to
+ * tokenB for the exact amount of tokenB.
+ */
+export async function computeTokenAToExactTokenB (tokenA: Contract, tokenB: Contract, exactTokenB: any, provider: any, opts: BestPriceOptions = {}): Promise<Trade[]> {
+  const uniTokenA: Token = await fromEthersToUniswapToken(tokenA, opts)
+  const uniTokenB: Token = await fromEthersToUniswapToken(tokenB, opts)
+
+  return await Base.computeTokenAToExactTokenB(uniTokenA, uniTokenB, exactTokenB, provider, opts)
+}
+
+/**
+ * Converts an ERC20 token as represented by ethers.Contract to a
+ * Uniswap.Token object.
+ *
+ * @param {Contract} token The token being converted.
+ * @param {BestPriceOptions} opts One or more options for the best path
+ * calculation. Overrides the default best price options.
+ * @returns A Uniswap.Token representation of the ERC20 token.
+ */
+export async function fromEthersToUniswapToken (token: Contract, opts: BestPriceOptions = {}): Promise<Token> {
+  const options: BestPriceOptions = { ...BEST_PRICE_OPTIONS, ...opts }
+
+  return new Token(
+    options.selectedChainId ?? CHAIN_ID_DEFAULT,
+    token.address,
+    await token.decimals(),
+    await token.symbol(),
+    await token.name()
+  )
+}
---
+++ test/ethers.spec.ts
@@ -0,0 +1,35 @@
+import * as dotenv from 'dotenv'
+import { ethers, Contract } from 'ethers'
+import ArtifactERC20 from '@uniswap/v2-core/build/IERC20.json'
+import { findBestPathExactTokenAToTokenB, findBestPathTokenAToExactTokenB } from '../src/wrappers/ethers'
+
+dotenv.config()
+
+jest.setTimeout(60000)
+
+describe('Best Path - Web3', () => {
+  let tokenA: Contract, tokenB: Contract
+  let provider: any
+
+  beforeAll(() => {
+    provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_URL ?? '')
+
+    tokenA = new ethers.Contract('0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', ArtifactERC20.abi, provider)
+
+    tokenB = new ethers.Contract('0xB4d930279552397bbA2ee473229f89Ec245bc365', ArtifactERC20.abi, provider)
+  })
+
+  test('should compute best path for exact token A to token B', async () => {
+    const path = await findBestPathExactTokenAToTokenB(tokenA, tokenB, '1000000000000000000', provider)
+
+    expect(path[0]).toEqual(tokenA.address)
+    expect(path[path.length - 1]).toEqual(tokenB.address)
+  })
+
+  test('should compute best path for token A to exact token B', async () => {
+    const path = await findBestPathTokenAToExactTokenB(tokenA, tokenB, '1000000000000000000', provider)
+
+    expect(path[0]).toEqual(tokenA.address)
+    expect(path[path.length - 1]).toEqual(tokenB.address)
+  })
+})
